50bee76ec4995f8bd928893d385e0560722529db
lukegarland <luke.garland01@gmail.com>
2020-04-09 15:52:33 -0600

    Add interface for message constants

---
+++ Milestone2/src/common/Messages.java
@@ -0,0 +1,12 @@
+package common;
+
+public interface Messages 
+{
+
+	public static final String getCatalogue = "GET_CAT";
+	public static final String searchCatalogue = "SEARCH_CAT";
+	public static final String searchStudentCourses = "GET_STU_COURSES";
+	public static final String addCourse = "ADD_COURSE";
+	public static final String removeCourse = "REM_COURSE";
+
+}
