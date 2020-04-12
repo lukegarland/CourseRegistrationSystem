a0ec8eefe8448732a53597847566bd206f3d2dbe
Cameron Faith <cmrnfaith@gmail.com>
2020-04-12 16:06:57 -0600

    Removed unnecessary content
    Extra imports on RegistrationApp removed
    Any extra TODO comments removed
    Code review completed
    A rename for RegistrationApp might be helpful as it's name is misleading
    Return to menu buttons still could be added
    textArea in main window could be resized

--- Milestone2/src/serverController/RegistrationApp.java
+++ Milestone2/src/serverController/RegistrationApp.java
@@ -1,12 +1,8 @@
 package serverController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputFilter;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Arrays;
 
